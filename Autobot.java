004dcc454c0cee4674a1cea8b87c29d02246ae24
Andrey Berillo <berillo.andrey750@gmail.com>
2026-05-23 15:40:15 +0700

    complete task 22

--- Autobot.java
+++ Autobot.java
@@ -1,4 +1,4 @@
-public class Autobot extends Transformer {
+public class Autobot extends Transformer implements Action {
     private String teamName;
     private final String eyeColor;
     private boolean kindness;
@@ -26,7 +26,28 @@ public class Autobot extends Transformer {
         return kindness;
     }
 
+    @Override
     public void transform() {
         System.out.println(getName() + " transforms into vehicles");
     }
+
+    // @Override
+    // public void run() {
+    // System.out.println(getName() + " is running");
+    // }
+
+    @Override
+    public void fire() {
+        System.out.println(getName() + " is firing");
+    }
+
+    @Override
+    public void charge() {
+        System.out.println(getName() + " is charging");
+    }
+
+    // @Override
+    // public String toString() {
+    // return "Transformer: " + getName();
+    // }
 }
--- MainApp.java
+++ MainApp.java
@@ -1,16 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class MainApp {
     public static void main(String[] args) {
         Autobot optimus = new Autobot("Optimus Prime", "Autobot");
         Decepticon megatron = new Decepticon("Megatron", "Decepticon");
 
+        List<Transformer> transformers = new ArrayList<>();
+        transformers.add(optimus);
+        transformers.add(megatron);
+
         optimus.run();
         optimus.fire();
         optimus.charge();
         optimus.transform();
+        System.out.println();
 
         megatron.run();
         megatron.fire();
         megatron.charge();
         megatron.transform();
+        System.out.println();
+
+        System.out.println("Cписок трансформеров:");
+        for (Transformer transformer : transformers) {
+            System.out.println(transformer);
+        }
     }
 }
